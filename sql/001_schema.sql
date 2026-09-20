c06eab826bac51320c0b49c54bcfab901c536c87
Leeevai <hassanmohseen22@gmail.com>
2026-09-20 17:18:22 -0400

    fix(sql::schema): add unique constrait over users::email

--- sql/001_schema.sql
+++ sql/001_schema.sql
@@ -6,7 +6,7 @@
 
 CREATE TABLE users (
     id             TEXT        PRIMARY KEY,
-    email          TEXT        NOT NULL,
+    email          TEXT        NOT NULL UNIQUE,
     password_hash  TEXT        NOT NULL,
     display_name   TEXT        NOT NULL,
     is_active      BOOLEAN     NOT NULL DEFAULT TRUE,
